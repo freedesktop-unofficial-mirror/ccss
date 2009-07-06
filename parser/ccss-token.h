f2fa291d90ee812c70d320bd2e5e48c48fa7cd3c
Robert Staudinger <robsta@gnome.org>
2009-07-06 15:10:24 +0200

    Add token enum.

---
+++ parser/ccss-token.h
@@ -0,0 +1,79 @@
+/* vim: set ts=8 sw=8 noexpandtab: */
+
+/* The `C' CSS Library.
+ * Copyright (C) 2008 Robert Staudinger
+ *
+ * This  library is free  software; you can  redistribute it and/or
+ * modify it  under  the terms  of the  GNU Lesser  General  Public
+ * License  as published  by the Free  Software  Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed  in the hope that it will be useful,
+ * but  WITHOUT ANY WARRANTY; without even  the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License  along  with  this library;  if not,  write to  the Free
+ * Software Foundation, Inc., 51  Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef CCSS_TOKEN_H
+#define CCSS_TOKEN_H
+
+typedef enum {
+	CDO = 1,
+	CDC,
+	INCLUDES,
+	DASHMATCH,
+
+	LBRACE,
+	PLUS,
+	GREATER,
+	COMMA,
+
+	STRING,
+	INVALID,
+
+	IDENT,
+
+	HASH,
+
+	IMPORT_SYM,
+	PAGE_SYM,
+	MEDIA_SYM,
+	CHARSET_SYM,
+
+	IMPORTANT_SYM,
+
+	EMS,
+	EXS,
+	LENGTH,
+	LENGTH,
+	LENGTH,
+	LENGTH,
+	LENGTH,
+	LENGTH,
+	ANGLE,
+	ANGLE,
+	ANGLE,
+	TIME,
+	TIME,
+	FREQ,
+	FREQ,
+	DIMENSION,
+
+	PERCENTAGE,
+	NUMBER,
+
+	URI,
+	URI,
+
+	FUNCTION
+} ccss_token_type_t;
+
+
+
+#endif /* CCSS_TOKEN_H */
+
